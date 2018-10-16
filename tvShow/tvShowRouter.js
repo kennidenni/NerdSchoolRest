3b45d8a1efbeec5ad2f96a4b0492dbcefad1fae1
Kenneth Apeland <kenneth.apeland@student.uib.no>
2018-10-16 19:39:02 +0200

    working find tv show

--- tvShow/tvShowRouter.js
+++ tvShow/tvShowRouter.js
@@ -7,4 +7,12 @@ const tvShowRouter = express.Router();
 tvShowRouter.get('/', (req, res) => {
     res.json(tvShowService.getAll());
 });
+
+tvShowRouter.route('/:tvShowId').get((req, res) => {
+    const id = req.params.tvShowId
+    const tvShow = tvShowService.getById(id) 
+    console.log("Fetching TV Show with id: " + id);
+    res.json(tvShow);
+});
+
 module.exports = tvShowRouter;
--- tvShow/tvShowService.js
+++ tvShow/tvShowService.js
@@ -1,10 +1,16 @@
+const TvShow = require('./TvShow');
 class TvShowService {
+  
   constructor() {
-    this.tvShows = [];
+    this.tvShows = [new TvShow(1, 'Mr.Robot', 'Drama'), new TvShow(2, 'Black Mirror', 'Drama')];
   }
   // (You didn't copy & paste this, did you?)
   getAll() {
     return this.tvShows;
+  }
+
+  getById(thisId){
+    return this.tvShows.find(tv => tv.id == thisId);
   }
 }
 module.exports = new TvShowService();
